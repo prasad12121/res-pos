74eff67ffe60e492d79f2274c0b8c83b755193eb
prasadpalase <prasadp.palase@gmail.com>
2025-12-03 10:53:41 +0530

    updated code for manifest for cache

--- src/service-worker.js
+++ src/service-worker.js
@@ -6,3 +6,19 @@ self.addEventListener("install", (event) => {
     console.log("Service Worker activated");
   });
   
+
+  self.addEventListener("fetch", (event) => {
+    event.respondWith(
+      caches.open("v1").then((cache) => {
+        return cache.match(event.request).then((response) => {
+          return (
+            response ||
+            fetch(event.request).then((networkResponse) => {
+              cache.put(event.request, networkResponse.clone());
+              return networkResponse;
+            })
+          );
+        });
+      })
+    );
+  });
